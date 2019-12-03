52cd2b42e63f49a0b4c1b0c50b019e9cabd265c2
Dzhoy Zuckerman <dzhoyz@gmail.com>
2019-12-03 14:47:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["dzhoy","burakevich","nazmulovich","zuckerman"]
+
 end
 
 def add_element_to_start_of_array(array, element)
